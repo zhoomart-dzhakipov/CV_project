f50efb91a68c7eeb9d556d10ff44b6284003f70e
Zhoomart <106585613+zhoomart-dzhakipov@users.noreply.github.com>
2025-07-05 22:24:05 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "ssh_public_key" {
   type = string
   description = "Public SSH Key"
 }
+
+variable "ssh_key_name" {
+  type = string
+  description = "The name of the existing SSH key in Hetzner Cloud"
+}
