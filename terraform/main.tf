b6e274f0ad946a76802389f618a047e09d413e0c
Zhoomart <106585613+zhoomart-dzhakipov@users.noreply.github.com>
2025-07-05 22:17:48 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,20 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
+data "hcloud_ssh_key" "existing" {
+  name = var.default-key  # имя ключа, уже добавленного в Hetzner
+}
+
 resource "hcloud_ssh_key" "default" {
-  name       = "protfolio-key-01"
+  name       = "default-key"
   public_key = var.ssh_public_key
 }
 
 resource "hcloud_server" "portfolio" {
   name        = "portfolio-server"
-  image       = "ubuntu-22.04"
-  server_type = "cx21"                      # можно cx11, cx21, cx31 и т.д.
-  location    = "nbg1"                      # nbg1, hel1, fsn1
+  image       = "ubuntu-24.04"
+  server_type = "cpx11"                      # можно cx11, cx21, cx31 и т.д.
+  location    = "hel1"                      # nbg1, hel1, fsn1
 
   ssh_keys = [hcloud_ssh_key.default.id]
 
